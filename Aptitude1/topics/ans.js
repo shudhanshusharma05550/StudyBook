7bd2189637bd174a384899a94a20e65898d8ea59
shudhanshusharma05550 <93518031+shudhanshusharma05550@users.noreply.github.com>
2022-09-15 17:16:01 +0530

    first commit

---
+++ Aptitude1/topics/ans.js
@@ -0,0 +1,165 @@
+function solution1(){
+    let para = document.getElementById('para1');
+    let btn = document.getElementById('sol1');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution2(){
+    let para = document.getElementById('para2');
+    let btn = document.getElementById('sol2');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution3(){
+    let para = document.getElementById('para3');
+    let btn = document.getElementById('sol3');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution4(){
+    let para = document.getElementById('para4');
+    let btn = document.getElementById('sol4');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution5(){
+    let para = document.getElementById('para5');
+    let btn = document.getElementById('sol5');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+            para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution6(){
+    let para = document.getElementById('para6');
+    let btn = document.getElementById('sol6');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution7(){
+    let para = document.getElementById('para7');
+    let btn = document.getElementById('sol7');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution8(){
+    let para = document.getElementById('para8');
+    let btn = document.getElementById('sol8');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution9(){
+    let para = document.getElementById('para9');
+    let btn = document.getElementById('sol9');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution10(){
+    let para = document.getElementById('para10');
+    let btn = document.getElementById('sol10');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution11(){
+    let para = document.getElementById('para11');
+    let btn = document.getElementById('sol11');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution12(){
+    let para = document.getElementById('para12');
+    let btn = document.getElementById('sol12');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution13(){
+    let para = document.getElementById('para13');
+    let btn = document.getElementById('sol13');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function solution14(){
+    let para = document.getElementById('para14');
+    let btn = document.getElementById('sol14');
+    if(para.style.display == 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'block';
+    }
+    else{
+        para.style.display = 'none';
+    }
+}
+function solution15(){
+    let para = document.getElementById('para15');
+    let btn = document.getElementById('sol15');
+    if(para.style.display != 'none'){
+        para.style.backgroundColor = 'SandyBrown';
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
